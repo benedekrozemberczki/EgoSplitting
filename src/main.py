82c8772edf43c0d5fb3ab38dde3c93fb1bf5d99e
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-12-06 21:18:48 +0000

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
-from parser import parameter_parser
+"""Running the clustering tool."""
+
+from param_parser import parameter_parser
 from ego_splitter import EgoNetSplitter
 from utils import tab_printer, graph_reader, membership_saver
 
 def main():
     """
-    Parsing command line parameters, creating EgoNets, creating a partition of the persona graph. Saving the memberships.
+    Parsing command line parameters, creating EgoNets.
+    Creating a partition of the persona graph. Saving the memberships.
     """
     args = parameter_parser()
     tab_printer(args)
     graph = graph_reader(args.edge_path)
-    splitter = EgoNetSplitter(graph, args.resolution)
-    splitter.create_egonets()
-    splitter.map_personalities()
-    splitter.create_persona_graph()
-    splitter.create_partitions()
+    splitter = EgoNetSplitter(args.resolution)
+    splitter.fit(graph)
     membership_saver(args.output_path, splitter.overlapping_partitions)
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+"""General data treatment utilities."""
+
 import json
 import torch
 import numpy as np
@@ -13,8 +15,9 @@ def tab_printer(args):
     """
     args = vars(args)
     keys = sorted(args.keys())
-    t = Texttable() 
-    t.add_rows([["Parameter", "Value"]] +  [[k.replace("_"," ").capitalize(),args[k]] for k in keys])
+    t = Texttable()
+    t.add_rows([["Parameter", "Value"]])
+    t.add_rows([[k.replace("_", " ").capitalize(), args[k]] for k in keys])
     print(t.draw())
 
 def graph_reader(path):
@@ -24,7 +27,7 @@ def graph_reader(path):
     :return graph: NetworkX object returned.
     """
     graph = nx.from_edgelist(pd.read_csv(path).values.tolist())
-    graph.remove_edges_from(graph.selfloop_edges())
+    graph.remove_edges_from(nx.selfloop_edges(graph))
     return graph
 
 def membership_saver(path, memberships):
@@ -33,5 +36,5 @@ def membership_saver(path, memberships):
     :param path: Output path.
     :param memberships: Membership dictionary.
     """
-    with open(path,"w") as f:
-        json.dump(memberships,f)
+    with open(path, "w") as f:
+        json.dump(memberships, f)
